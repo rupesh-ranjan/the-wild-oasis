a0945d92b090aad4a9c1f296bb24356523002606
Rupesh Ranjan <2068799@cognizant.com>
2024-04-16 13:46:14 +0530

    global styled with styled component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Input from "./ui/Input";
+import H1 from "./ui/H1";
+import StyledApp from "./ui/StyledApp";
+import Button from "./ui/Button";
+
 function App() {
-  return <div>Hello from The Wild Oasis</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello from The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <Input type="number" placeholder="No of guest" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
---
+++ src/ui/StyledApp.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  background-color: #8a8585;
+  padding: 20px;
+`;
+
+export default StyledApp;
