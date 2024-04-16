98c721c18d49be9dad9907b56941eea112683cc7
Rupesh Ranjan <2068799@cognizant.com>
2024-04-16 14:26:46 +0530

    resusable styled componets created {Row, Button, Heading}

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
 import GlobalStyles from "./styles/GlobalStyles";
 import Input from "./ui/Input";
-import H1 from "./ui/H1";
+import Heading from "./ui/Heading";
 import StyledApp from "./ui/StyledApp";
 import Button from "./ui/Button";
+import Row from "./ui/Row";
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Hello from The Wild Oasis</H1>
-        <Button onClick={() => alert("Check in")}>Check in</Button>
-        <Button onClick={() => alert("Check out")}>Check out</Button>
-        <Input type="number" placeholder="No of guest" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hello from The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Hello from The Wild Oasis</Heading>
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button
+                variation="seconday"
+                size="small"
+                onClick={() => alert("Check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <div>
+              <Heading as="h3">Form</Heading>
+              <Input type="number" placeholder="No of guest" />
+            </div>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
